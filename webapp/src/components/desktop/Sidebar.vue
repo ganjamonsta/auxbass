ae790d9461fe9a6e88cff8d3928350b49afa823a
asuka <asukanitro@gmail.com>
2026-02-02 06:48:49 +0300

    feat: add albums link to sidebar navigation

--- webapp/src/components/desktop/Sidebar.vue
+++ webapp/src/components/desktop/Sidebar.vue
@@ -26,6 +26,14 @@
         <span>Любимое</span>
         <span v-if="likedCount" class="nav-count">{{ formatCount(likedCount) }}</span>
       </router-link>
+
+      <router-link to="/albums" class="nav-item" :class="{ active: isActive('/albums') }">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 14.5c-2.49 0-4.5-2.01-4.5-4.5S9.51 7.5 12 7.5s4.5 2.01 4.5 4.5-2.01 4.5-4.5 4.5zm0-5.5c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1z"/>
+        </svg>
+        <span>Альбомы</span>
+        <span v-if="albums.length" class="nav-count">{{ albums.length }}</span>
+      </router-link>
     </nav>
 
     <!-- Playlists Section -->
@@ -97,13 +105,7 @@
         <span v-if="artistCount" class="nav-count">{{ formatCount(artistCount) }}</span>
       </router-link>
 
-      <router-link to="/albums" class="nav-item" :class="{ active: isActive('/albums') }">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 14.5c-2.49 0-4.5-2.01-4.5-4.5S9.51 7.5 12 7.5s4.5 2.01 4.5 4.5-2.01 4.5-4.5 4.5zm0-5.5c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1z"/>
-        </svg>
-        <span>Альбомы</span>
-        <span v-if="albums.length" class="nav-count">{{ albums.length }}</span>
-      </router-link>
+
     </div>
 
     <!-- User Section (bottom) -->
