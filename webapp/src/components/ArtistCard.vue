d903cfa7c6e1992d94237e2d2d32d6056e37571b
asuka <asukanitro@gmail.com>
2026-02-04 06:10:23 +0300

    feat: disable shuffle button when shuffling is active and add isShuffling prop to ArtistCard component

--- webapp/src/components/ArtistCard.vue
+++ webapp/src/components/ArtistCard.vue
@@ -23,7 +23,7 @@
             <path d="M8 5v14l11-7z"/>
           </svg>
         </button>
-        <button class="action-btn shuffle-btn" @click="shufflePlay">
+        <button class="action-btn shuffle-btn" @click="shufflePlay" :disabled="isShuffling">
           <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
             <path d="M10.59 9.17L5.41 4 4 5.41l5.17 5.17 1.42-1.41zM14.5 4l2.04 2.04L4 18.59 5.41 20 17.96 7.46 20 9.5V4h-5.5zm.33 9.41l-1.41 1.41 3.13 3.13L14.5 20H20v-5.5l-2.04 2.04-3.13-3.13z"/>
           </svg>
@@ -103,7 +103,7 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import TrackItem from './TrackItem.vue'
 import { formatPlayCount, getCoverUrl, CoverSize } from '@/utils'
 
@@ -113,7 +113,8 @@ const props = defineProps({
     required: true
   },
   currentTrackId: Number,
-  isPlaying: Boolean
+  isPlaying: Boolean,
+  isShuffling: Boolean
 })
 
 const emit = defineEmits(['play', 'menu', 'like', 'openAlbum', 'openPlaylist', 'playAll', 'shuffleAll', 'albumContextmenu'])
