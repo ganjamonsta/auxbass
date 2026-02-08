90fcfadf792b4d70fe65cb86d1e3152d13aa2c6a
asuka <asukanitro@gmail.com>
2026-02-08 17:03:03 +0300

    fix: remove unused useAuth composable from index exports

--- webapp/src/composables/index.js
+++ webapp/src/composables/index.js
@@ -3,7 +3,6 @@
  * Re-exports all composables for convenient imports
  */
 
-export { useAuth } from './useAuth'
 export { useNavigation } from './useNavigation'
 export { useSearch } from './useSearch'
 export { usePullToRefresh } from './usePullToRefresh'
